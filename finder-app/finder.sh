2fa9baa3f096d65e046543e13ecf22e7bfceefd7
João Teodoro <joao.v.teodoro@usp.br>
2025-05-08 21:28:09 -0300

    Style alteration

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -9,7 +9,6 @@ elif [ ! -d $1 ]; then
     echo "$filesdir is not a directory"
     exit 1 
 fi
-
 num_match=$(grep -r $1 -e $2 | wc -l)
 num_files=$(ls $1 | wc -w)
 echo "The number of files are $num_files and the number of matching lines are $num_match"
